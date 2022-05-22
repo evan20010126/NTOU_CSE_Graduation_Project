72de8e0ee7906d4a0cbc1711a815438e7890c43c
evan <55246608+evan20010126@users.noreply.github.com>
2022-05-22 14:12:41 +0000

    Add files via upload

---
+++ edit_excel.py
@@ -0,0 +1,15 @@
+from asyncio.windows_events import NULL
+import openpyxl
+from openpyxl import Workbook
+
+file_name = "Summary_file.xlsx"
+wkbook = openpyxl.load_workbook(file_name)
+wksheet = wkbook["Sheet1"]
+
+# print(wksheet.max_row)
+for i in range(2, wksheet.max_row+1):
+    for j in range(2, wksheet.max_column+1):
+        if wksheet.cell(row=i, column=j).value == None:
+            wksheet.cell(row=i, column=j).value = 0
+
+wkbook.save("Summary2.xlsx")
