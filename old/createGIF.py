646f4286f9449c2ff382db00086b6af24124d46f
00853029 <edmund1119@gmail.com>
2022-07-17 01:35:46 +0800

    0

--- old/createGIF.py
+++ old/createGIF.py
@@ -1,14 +1,13 @@
 import os
 import imageio
-num = int(input())
 def create_gif(source, name, duration):
     frames = []
     for img in source:
         frames.append(imageio.imread(img))
     imageio.mimsave(name, frames, 'GIF', duration = duration)
-    print("處裡完成")
-path = os.chdir(f".//fig{num}")
+    print("success")
+path = os.chdir("..//path_img//test")
 pic_list = os.listdir()
-gif_name = f"result{num}.gif"
-duration_time = 0.1
+gif_name = f"test.gif"
+duration_time = 0.2
 create_gif(pic_list, gif_name, duration_time)
