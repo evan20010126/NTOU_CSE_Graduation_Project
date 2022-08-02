4cd31dc591cafc4e6aaccf46bd619ea3c0018998
yumi <yumi876122@gmail.com>
2022-08-02 14:25:06 +0800

    ok

--- old/edit_csv_stuff_zero.py
+++ old/edit_csv_stuff_zero.py
@@ -31,8 +31,10 @@ if START:
 
 # 00853029
 
+
 summary_file_version = "11_2"  # !改這裡就好
 # summary_file_version = "Friend"  # !改這裡就好
+
 
 largest_column_count = 27301  # !還有這裡
 csv_file = f'output.csv'
