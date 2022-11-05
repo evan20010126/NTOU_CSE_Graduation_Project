47dc079827876294786a8b9f2d98c9957b52e2f2
yumi <yumi876122@gmail.com>
2022-11-05 23:58:03 +0800

    Update timeseries_classification_from_lstm.py

--- vector/timeseries_classification_from_lstm.py
+++ vector/timeseries_classification_from_lstm.py
@@ -65,6 +65,62 @@ pose_sequence = [(0, 12), (0, 11),
 point_number = len(hand_sequence*2) + len(pose_sequence)
 
 
+def to_vector_evanVersion(df):
+    data = df.to_numpy()
+    new_data = np.array(list())
+    row_length = 0
+
+    for row in data:
+        temp_row = np.array(list())
+
+        if row[0] == "salty":
+            temp_row = np.append(temp_row, [0.0, ])
+        elif row[0] == "snack":
+            temp_row = np.append(temp_row, [1.0, ])
+        elif row[0] == "bubbletea":
+            temp_row = np.append(temp_row, [2.0, ])
+        elif row[0] == "dumpling":
+            temp_row = np.append(temp_row, [3.0, ])
+        elif row[0] == "spicy":
+            temp_row = np.append(temp_row, [4.0, ])
+        elif row[0] == "sour":
+            temp_row = np.append(temp_row, [5.0, ])
+        elif row[0] == "sweet":
+            temp_row = np.append(temp_row, [6.0, ])
+        elif row[0] == "yummy":
+            temp_row = np.append(temp_row, [7.0, ])
+
+        # pose: 23個點 left/right:各21個點 23+21*2=65
+        vector = row[1:]
+        vector = vector.reshape((vector.shape[0])//130, 65, 2)  # (幾偵, 點, xy)
+        for img in vector:  # 迭代每一偵
+            pose_points = img[0:23]
+            left_hand_points = img[23:23+21]
+            right_hand_points = img[23+21:23+21+21]
+
+            # # 加上不是向量的點
+            # temp_row = np.append(temp_row, pose_points[0])
+            # temp_row = np.append(temp_row, pose_points[15])
+            # temp_row = np.append(temp_row, pose_points[16])
+            # #
+
+            for p1, p2 in pose_sequence:
+                temp_row = np.append(
+                    temp_row, pose_points[p2] - pose_points[p1])
+
+            for p1, p2 in hand_sequence:
+                temp_row = np.append(
+                    temp_row, left_hand_points[p2] - left_hand_points[p1])
+
+            for p1, p2 in hand_sequence:
+                temp_row = np.append(
+                    temp_row, right_hand_points[p2] - right_hand_points[p1])
+        # print(temp_row.shape) # 1787
+        row_length = temp_row.shape[0]
+        new_data = np.append(new_data, temp_row)
+    new_data = new_data.reshape(data.shape[0], row_length)
+
+
 def split_target_evanVersion(new_data_df):
     new_data = new_data_df.to_numpy()
     y = new_data[:, 0]
@@ -153,9 +209,9 @@ leave_idx = int(sys.argv[1])  # 0 ~ 15
 evan, edmund, yumi,\
     friend_1, friend_2, friend_3, friend_4, friend_5, friend_6,\
     friend_7, friend_8, friend_9, friend_10, friend_11, friend_12, friend_13\
-    = share_function.load_point_data()
+    = share_function.load_vector_data()
 
-all_person_pd_points = [evan, yumi, edmund, friend_1, friend_2, friend_3, friend_4, friend_5,
+all_person_pd_vector = [evan, yumi, edmund, friend_1, friend_2, friend_3, friend_4, friend_5,
                         friend_6, friend_7, friend_8, friend_9, friend_10, friend_11, friend_12, friend_13]
 
 del evan, edmund, yumi, friend_1, friend_2, friend_3, friend_4, friend_5, friend_6,\
@@ -164,52 +220,54 @@ del evan, edmund, yumi, friend_1, friend_2, friend_3, friend_4, friend_5, friend
 IS_EMPTY = True
 # train_vectors = -999
 # test_vectors = -999
-train_points = -999
-test_points = -999
+train_vector = -999
+test_vector = -999
 for i in range(16):
     if (i != leave_idx):
         if not IS_EMPTY:
-            # train_vectors = pd.concat([train_vectors, all_person_pd_vector[i]])
-            train_points = pd.concat([train_points, all_person_pd_points[i]])
+            train_vectors = pd.concat([train_vectors, all_person_pd_vector[i]])
+            # train_points = pd.concat([train_points, all_person_pd_points[i]])
         else:
-            # train_vectors = all_person_pd_vector[i]
-            train_points = all_person_pd_points[i]
+            train_vectors = all_person_pd_vector[i]
+            # train_points = all_person_pd_points[i]
             IS_EMPTY = False
         if (leave_idx == -1):
-            # test_vectors = all_person_pd_vector[i]
-            test_points = all_person_pd_points[i]
+            test_vectors = all_person_pd_vector[i]
+            # test_points = all_person_pd_points[i]
     else:
-        # test_vectors = all_person_pd_vector[i]
-        test_points = all_person_pd_points[i]
+        test_vectors = all_person_pd_vector[i]
+        # test_points = all_person_pd_points[i]
 
-train_points = share_function.label_to_float(train_points)
+# train_points = share_function.label_to_float(train_points)
 
-test_points = share_function.label_to_float(test_points)
+# test_points = share_function.label_to_float(test_points)
 
-# del all_person_pd_vector
-del all_person_pd_points
+del all_person_pd_vector
+# del all_person_pd_points
 
 #! shuffle
-train_points = share_function.shuffle(points=train_points)
+# train_points = share_function.shuffle(points=train_points)
+train_vectors = share_function.shuffle(train_vectors)
+
 # train = train.sample(frac=1).reset_index(drop=True)
 # test = test.sample(frac=1).reset_index(drop=True)
 
 
-x_train_points, y_train_points = \
+# x_train_points, y_train_points = \
+# split_target_evanVersion(
+# train_points)  # origin: x_train, y_train = split_target(train)
+
+x_train_vectors, y_train_vectors = \
     split_target_evanVersion(
-        train_points)  # origin: x_train, y_train = split_target(train)
+        train_vectors)  # origin: x_train, y_train = split_target(train)
 
-# x_train_vectors, y_train_vectors = \
+# x_test_points, y_test_points = \
 #     split_target_evanVersion(
-#         train_vectors)  # origin: x_train, y_train = split_target(train)
+#         test_points)  # origin: x_train, y_train = split_target(train)
 
-x_test_points, y_test_points = \
+x_test_vectors, y_test_vectors = \
     split_target_evanVersion(
-        test_points)  # origin: x_train, y_train = split_target(train)
-
-# x_test_vectors, y_test_vectors = \
-#     split_target_evanVersion(
-#         test_vectors)  # origin: x_train, y_train = split_target(train)
+        test_vectors)  # origin: x_train, y_train = split_target(train)
 # .
 # x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], 1))
 # x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
@@ -217,33 +275,34 @@ x_test_points, y_test_points = \
 
 # y_train[y_train == -1] = 0
 # y_test[y_test == -1] = 0
-x_train_points = np.asarray(x_train_points).astype(np.float32)
-y_train_points = np.asarray(y_train_points).astype(np.float32)
+# x_train_points = np.asarray(x_train_points).astype(np.float32)
+# y_train_points = np.asarray(y_train_points).astype(np.float32)
 
-# x_train_vectors = np.asarray(x_train_vectors).astype(np.float32)
-# y_train_vectors = np.asarray(y_train_vectors).astype(np.float32)
+x_train_vectors = np.asarray(x_train_vectors).astype(np.float32)
+y_train_vectors = np.asarray(y_train_vectors).astype(np.float32)
 
-x_test_points = np.asarray(x_test_points).astype(np.float32)
-y_test_points = np.asarray(y_test_points).astype(np.float32)
+# x_test_points = np.asarray(x_test_points).astype(np.float32)
+# y_test_points = np.asarray(y_test_points).astype(np.float32)
 
-# x_test_vectors = np.asarray(x_test_vectors).astype(np.float32)
-# y_test_vectors = np.asarray(y_test_vectors).astype(np.float32)
-
-
-num_classes = len(np.unique(y_train_points))
+x_test_vectors = np.asarray(x_test_vectors).astype(np.float32)
+y_test_vectors = np.asarray(y_test_vectors).astype(np.float32)
 
 
-x_train_points = x_train_points.flatten().reshape(
-    x_train_points.shape[0], x_train_points.shape[1]//130, 130)
+# num_classes = len(np.unique(y_train_points))
+num_classes = len(np.unique(y_train_vectors))
 
-x_test_points = x_test_points.flatten().reshape(
-    x_test_points.shape[0], x_test_points.shape[1]//130, 130)
 
-# x_train_vectors = x_train_vectors.flatten().reshape(
-#     x_train_vectors.shape[0], (x_train_vectors.shape[1]//(point_number*2)), (point_number*2))
+# x_train_points = x_train_points.flatten().reshape(
+#     x_train_points.shape[0], x_train_points.shape[1]//130, 130)
 
-# x_test_vectors = x_test_vectors.flatten().reshape(
-#     x_test_vectors.shape[0], (x_test_vectors.shape[1]//(point_number*2)), (point_number*2))
+# x_test_points = x_test_points.flatten().reshape(
+#     x_test_points.shape[0], x_test_points.shape[1]//130, 130)
+
+x_train_vectors = x_train_vectors.flatten().reshape(
+    x_train_vectors.shape[0], (x_train_vectors.shape[1]//(point_number*2)), (point_number*2))
+
+x_test_vectors = x_test_vectors.flatten().reshape(
+    x_test_vectors.shape[0], (x_test_vectors.shape[1]//(point_number*2)), (point_number*2))
 
 """## Load the data: the FordA dataset
 
@@ -399,8 +458,10 @@ def make_model(input_shape_point, input_shape_vector=0):
 # print(x_train.shape[1]//(point_number*2))
 
 
+# model = make_model(
+#     input_shape_point=x_train_points.shape[1:])
 model = make_model(
-    input_shape_point=x_train_points.shape[1:])
+    input_shape_point=x_train_vectors.shape[1:])
 keras.utils.plot_model(model, show_shapes=True)
 
 # 1. 錄影怎麼錄影
@@ -446,9 +507,18 @@ model.compile(
     metrics=["sparse_categorical_accuracy"],
 )
 
+# history = model.fit(
+#     x_train_points,
+#     y_train_points,
+#     batch_size=batch_size,
+#     epochs=epochs,
+#     callbacks=callbacks,
+#     validation_split=0.2,
+#     verbose=1,
+# )
 history = model.fit(
-    x_train_points,
-    y_train_points,
+    x_train_vectors,
+    y_train_vectors,
     batch_size=batch_size,
     epochs=epochs,
     callbacks=callbacks,
@@ -461,9 +531,10 @@ history = model.fit(
 model = keras.models.load_model("Lstm_best_model.h5")
 model.save(f'auto_leave_person/{leave_idx}/Lstm_best_model.h5')  # 另存一份
 
+# test_loss, test_acc = model.evaluate(
+#     x_test_points, y_test_points)
 test_loss, test_acc = model.evaluate(
-    x_test_points, y_test_points)
-
+    x_test_vectors, y_test_vectors)
 
 # history.loss_plot('epoch')
 print("Test accuracy", test_acc)
@@ -555,11 +626,13 @@ def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None
 last_conv_layer_name = model.layers[-3].name
 # print(model.layers[-3].name)
 
-# img_array_vectors = x_test_vectors[1][tf.newaxis, ...]
-img_array_points = x_test_points[1][tf.newaxis, ...]
+img_array_vectors = x_test_vectors[1][tf.newaxis, ...]
+# img_array_points = x_test_points[1][tf.newaxis, ...]
 
 heatmap = make_gradcam_heatmap(
-    img_array_points, model, last_conv_layer_name, pred_index=0)
+    img_array_vectors, model, last_conv_layer_name, pred_index=0)
+# heatmap = make_gradcam_heatmap(
+#     img_array_points, model, last_conv_layer_name, pred_index=0)
 print(heatmap.shape)  # 19偵
 # plt.matshow(heatmap)
 # plt.show()
@@ -573,10 +646,16 @@ print(heatmap.shape)  # 19偵
 
 ################################################################################################
 # confusion matrix
+
+
+# predict_ans = np.argmax(model.predict(
+#     x_test_points), axis=-1)  # *  argmax 找最大值的index
+# cm = tf.math.confusion_matrix(
+#     y_test_points, predict_ans).numpy().astype(np.float32)
 predict_ans = np.argmax(model.predict(
-    x_test_points), axis=-1)  # *  argmax 找最大值的index
+    x_test_vectors), axis=-1)  # *  argmax 找最大值的index
 cm = tf.math.confusion_matrix(
-    y_test_points, predict_ans).numpy().astype(np.float32)
+    y_test_vectors, predict_ans).numpy().astype(np.float32)
 print(cm)
 print(cm.shape[0])
 print(cm.shape[1])
